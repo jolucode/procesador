49a7c549ac94db0bd6ac48377fafed0d92acec86
jolucode <157257214+jolucode@users.noreply.github.com>
2025-05-20 01:14:40 -0500

    fix: procesadorCloud-2025.05.12.jar campo comentario se mantiene

--- src/main/java/service/cloud/request/clientRequest/utils/JsonUtils.java
+++ src/main/java/service/cloud/request/clientRequest/utils/JsonUtils.java
@@ -5,14 +5,33 @@ import com.google.gson.GsonBuilder;
 import service.cloud.request.clientRequest.config.StringCleaningTypeAdapterFactory;
 
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import service.cloud.request.clientRequest.config.TransacctionDTODeserializer;
+import service.cloud.request.clientRequest.dto.dto.TransacctionDTO;
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
 
 public class JsonUtils {
 
-    // Gson con adaptador personalizado que limpia strings al deserializar
     private static final Gson gson = new GsonBuilder()
-            .registerTypeAdapterFactory(new StringCleaningTypeAdapterFactory())
+            .registerTypeAdapterFactory(new StringCleaningTypeAdapterFactory()) // limpieza general
+            .registerTypeAdapter(TransacctionDTO.class, new TransacctionDTODeserializer()) // excepción FE_Comentario
+            .registerTypeAdapter(TransacctionDTO[].class, new JsonDeserializer<TransacctionDTO[]>() {
+                @Override
+                public TransacctionDTO[] deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+                        throws JsonParseException {
+                    JsonArray jsonArray = json.getAsJsonArray();
+                    TransacctionDTO[] array = new TransacctionDTO[jsonArray.size()];
+                    TransacctionDTODeserializer dtoDeserializer = new TransacctionDTODeserializer();
+
+                    for (int i = 0; i < jsonArray.size(); i++) {
+                        array[i] = dtoDeserializer.deserialize(jsonArray.get(i), TransacctionDTO.class, context);
+                    }
+
+                    return array;
+                }
+            })
             .create();
 
     // Serializa un objeto a JSON (sin limpieza)
@@ -20,9 +39,10 @@ public class JsonUtils {
         return gson.toJson(obj);
     }
 
-    // Deserializa y limpia strings en el proceso
+    // Deserializa un objeto desde JSON
     public static <T> T fromJson(String json, Class<T> clazz) {
         return gson.fromJson(json, clazz);
     }
 }
+
 
